e5acfab158441502536c3284d8e841767ef73778
“uihalagedara” <it20224820@my.sliit.lk>
2022-03-14 16:01:29 +0530

    OnUpdate added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -26,16 +26,34 @@ const Modal = () => {
     }
   };
 
+  const OnUpdate = async (e) => {
+    e.preventDefault();
+
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, incorrect credentials.');
+    }
+  };
+
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Chat Application Demonstration </h1>
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Start chatting</span>
+              <span>Start test chat</span>
             </button>
           </div>
         </form>
