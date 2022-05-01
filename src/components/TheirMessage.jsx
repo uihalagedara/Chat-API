70cb1d841dad414e0c63e71a82f298258c5b0a14
Upendra Ihalagedara <78894119+uihalagedara@users.noreply.github.com>
2022-05-01 20:31:32 +0530

    Update TheirMessage.jsx

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -24,6 +24,29 @@ const TheirMessage = ({ lastMessage, message }) => {
             </div>
           )}
       </div>
+      
+       <div className="message">
+        {isFirstMessageByUser && (
+          <div
+            className="message"
+            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+          />
+        )}
+        {message.attachments && message.attachments.length > 0
+          ? (
+            <img
+              src={message.attachments[0].file}
+              alt="message-attachment"
+              className="message-image"
+              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            />
+          )
+          : (
+            <div className="message" style={{ float: 'left', backgroundColor: '#CABCEC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+              {message.text}
+            </div>
+          )}
+      </div>
     );
   };
   
